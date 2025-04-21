4364ec309b85ab3d68b7208bf2afb671c53059ed
chimp <tysonjjenkins.github@gmail.com>
2025-04-20 20:51:14 -0600

    Create dark mode color scheme

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import '../app.css';
   import docker from "$lib/assests/docker.png"
-  import home from "$lib/assests/home.png"
+  import home from "$lib/assests/home-light.png"
+  import {page} from "$app/state"
 	let { children } = $props();
 </script>
 
@@ -10,19 +11,28 @@
 	  <div class="p-[15px] text-[1.5rem]">
         <h1 class="text-[24px]"><b>TYSONCLOUD-prototype</b></h1>
 		  </div>
-		  <div>
-        <div class="flex p-[15px] text-[1.5rem]">
-          <div class="flex">
-            <img src={home} alt="Home icon" width="24px" height="24px"/>
-			      <a class="text-[16px]" href="/">Home</a>
+		  <div class="mr-[24px]">
+        <div class="flex items-center text-[1.5rem]">
+          <a href="/">
+          <div class="flex items-center rounded-lg background pt-[5px] pb-[5px] pl-[16px]" aria-current={page.url.pathname === "/"}>
+            <img class="color-white" src={home} alt="Home icon" width="32px" height="32px"/>
+			      <p class="ml-[16px] pr-[90px] text-[16px]" >Home</p>
           </div>
-          <div class="flex ml-[15px]">
-            <img src={docker} alt="Docker logo" width="24px" height="24px"/>
-			      <a class="text-[16px]" href="/containers">Containers</a>
+          </a>
+          <a href="/containers" >
+          <div class="flex items-center rounded-lg background pt-[5px] pb-[5px] pl-[16px]" aria-current={page.url.pathname === "/containers"}>
+            <img src={docker} alt="Docker logo" width="32px" height="32px"/>
+			      <p class="ml-[16px] pr-[48px] text-[16px]" >Containers</p>
         </div>
-      <!-- </div> -->
+        </a>
 		</div>
 	</div>
 </nav>
 
+
+<style>
+  .background[aria-current=true] {
+    background-color: #272727
+  }
+</style>
 {@render children()}
