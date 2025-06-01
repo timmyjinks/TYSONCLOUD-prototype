a4f0c14ce798ad5fee97b59384ec52dcf64efb55
chimp <tysonjjenkins.github@gmail.com>
2025-06-01 15:56:46 -0600

    Add update container option

--- frontend/src/routes/container/[name]/+page.svelte
+++ frontend/src/routes/container/[name]/+page.svelte
@@ -1,68 +1,52 @@
 <script lang="ts">
   import {page} from "$app/state"
-  import {goto} from "$app/navigation"
+	import { enhance } from '$app/forms';
+  import {Switch} from "$lib/components/ui/switch/index"
   import Button from "$lib/components/Button.svelte"
-  import {seletectedContainer} from "$lib/store"
+  import {selectedContainer} from "$lib/store"
   import deletes from "$lib/assests/delete.png"
   const name = page.url.pathname.split("/")[2]
-  console.log($seletectedContainer)
-  let loading = $state(false);
+  let loading = false;
+  let update_image = false;
+  let error = "";
+  export let form;
 
-async function updateContainer(e: SubmitEvent) {
-  loading = true;
-  e.preventDefault()
-  const form = e.currentTarget as HTMLFormElement;
-  const formData = new FormData(form);
-
-  const new_name = formData.get("new_name")
-  const image = formData.get("image")
-  const update_image = formData.get("update_image") ? true : false;
-  console.log(update_image)
-  const data = {"id": $seletectedContainer?.id, "new_name": new_name, "image": image, "update_image": update_image};
-
-  await fetch(`/api/container/${name}`, 
-    {
-      method: "PUT",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(data)
-    }
-  )
-  goto("/containers")
-}
-
-
-async function deleteContainer() {
-  await fetch(`/api/container/${name}`,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({"id": $seletectedContainer?.id})
-    }
-  )
-  goto("/containers")
-}
-
+  $: if(form?.error) {
+    loading = false;
+  }
+console.log($selectedContainer)
 
 </script>
 <section class="w-[80%] m-auto mt-[20px]">
   <div class="flex items-center justify-between">
  <b><h1 class="text-[1.5rem] m-[15px]">{name}</h1></b>
-  <button onclick={deleteContainer}><img src={deletes} alt="cook" width="24px" height="24px" class="text-right"/></button>
+  <form method="post" action="?/delete">
+  <input type="hidden" name="id" value={$selectedContainer?.id}/>
+  <button type="submit"><img src={deletes} alt="cook" width="24px" height="24px" class="text-right"/></button>
   </div>
-  <form onsubmit={updateContainer} class="border p-[15px]"  method="post">
+  <form action="?/update" use:enhance={() => {loading = true;}} class="border p-[15px]"  method="post">
   <div class="flex items-center">
-  <label class="text-nowrap" for="new_name">Container Name: </label>
+  <label class="text-nowrap" for="new_name">Container Name:</label>
   <input name="new_name" class="bg-[#272727] w-[100%] m-[15px]" type='text' placeholder="e.g. Nextjs App"><br>
+  <input type="hidden" name="id" value={$selectedContainer?.id}/>
   </div>
   <div class="flex items-center">
   <label class="text-nowrap" for="image">Image: </label>
-  <input class="bg-[#272727] w-[100%] m-[15px]" name="image" type='text' placeholder="e.g. my-image:my-tag"><br>
-  <label for="update-image">Update container</label>
-  <input type="checkbox" value=true name="update_image">
+  <input class="bg-[#272727] w-[100%] m-[15px]" name="image" type='text' placeholder="e.g. my-image:my-tag"><br/>
   </div>
-
+  <div class="flex items-center">
+    <label for="update-image">Update container</label>
+    <Switch bind:checked={update_image} id="update-image" class="m-[15px]"/>
+    <input type="hidden" name="update_image" value={update_image ? 'true' : 'false' } hidden/>
+  </div>
+  <div class="flex items-center">
+    <a class="mt-[15px] hover:text-red-500" href={"https://mangomongo-" + name + ".tysonjenkins.dev"}>{"https://mangomongo-" + name + ".tysonjenkins.dev"}</a>
+  </div>
+  <br/>
+  {#if form?.error}
+    <p>{form?.error}</p>
+  {/if}
+  <p>{error}</p>
   {#if loading}
     <Button loading={loading} content="..."/>
   {:else}
