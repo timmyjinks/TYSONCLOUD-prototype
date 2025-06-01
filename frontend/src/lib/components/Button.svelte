794f5592f310d965f0e36a3c1ed1c805b31a9c51
chimp <tysonjjenkins.github@gmail.com>
2025-06-01 15:54:13 -0600

    Update button loading parameter

--- frontend/src/lib/components/Button.svelte
+++ frontend/src/lib/components/Button.svelte
@@ -2,4 +2,4 @@
   let {content, loading} = $props()
 </script>
 
-<button class="bg-[#ed2939] text-black pr-[15px] pl-[15px] pt-[5px] pb-[5px] m-[15px] rounded-md" disabled={loading} type="submit">{content}</button>
+<button class="bg-[#ed2939] text-black pr-[15px] pl-[15px] pt-[5px] pb-[5px] mt-[15px] mb-[10px]  rounded-md" disabled={loading} type="submit">{content}</button>
