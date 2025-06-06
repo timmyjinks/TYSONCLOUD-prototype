c3bee59a75d73f00208582458e10d87079723c63
chimp <tysonjjenkins.github@gmail.com>
2025-06-06 12:06:39 -0600

    Add chart templates

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import Card from '$lib/components/Card.svelte';
-  import Chart from "$lib/components/Chart.svelte"
+  import DoughnutChart from "$lib/components/DoughnutChart.svelte"
   import container from "$lib/assests/container.png"
   import network from "$lib/assests/network.png"
   import image from "$lib/assests/images.png"
   let {data} = $props()
-console.log(data)
-</script>
+  let data_array = [];
+  data_array.push({resource: "containers", usage: data.container_info.containers})
+  data_array.push({resource: "networks", usage: data.container_info.networks})
+  data_array.push({resource: "images", usage: data.container_info.images})
 
-<section class="w-[80%] m-auto mt-[20px]">
+</script>
+<section class="w-[80%]  mt-[20px] m-auto">
   <b><h1 class="text-[1.5rem] m-[15px]">Environment</h1></b>
   <div class="flex flex-wrap justify-between">
-  <Card title="Containers" img={container} amount={data.container_info.containers}/>
-  <Card title="Networks" img={network} amount={data.container_info.networks}/>
-  <Card title="Images" img={image} amount={data.container_info.images}/>
+    <Card title="Containers" img={container} amount={data.container_info.containers}/>
+    <Card title="Networks" img={network} amount={data.container_info.networks}/>
+    <Card title="Images" img={image} amount={data.container_info.images}/>
   </div>
-  <Chart/>
 </section>
